520be9b4e04180482dc19e0107a2c8b8e8e123ee
Oscar Linderholm <osm@recv.se>
2019-05-04 17:45:17 +0200

    Fix clipboard

--- x.c
+++ x.c
@@ -631,6 +631,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -647,8 +649,8 @@ brelease(XEvent *e)
 		return;
 	}
 
-	if (e->xbutton.button == Button2)
-		selpaste(NULL);
+	if (e->xbutton.button == Button2 || e->xbutton.button == Button3)
+		clippaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
